60bd017f8b3eb1445dc9c77066c4b03a179f534a
Ethan Keller <ethan.keller@gmail.com>
2018-10-31 10:35:47 -0400

    try yet another travis build

--- .travis/git_push.sh
+++ .travis/git_push.sh
@@ -75,7 +75,7 @@ if [ "$git_remote" = "" ]; then # git remote not defined
 
     if [ "$GIT_TOKEN" = "" ]; then
         echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
-        git remote add origin https://github.com/${git_user_id}/${git_repo_id}.git
+        git remote add origin https://github.com/${git_org}/${git_repo_id}.git
     else
         git remote add origin https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git
     fi
