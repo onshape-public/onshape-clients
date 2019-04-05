7579caaafc364d60bf5d0a52a70345b72109f94c
Ethan Keller <ethan.keller@gmail.com>
2019-04-05 15:58:50 -0400

    more work on deploy docs for python dir location

--- python/docs.sh
+++ python/docs.sh
@@ -12,8 +12,9 @@ make_and_test_documentation (){
 # To be run after tests pass and build generated
 deploy_to_gh_pages(){
 	git checkout gh-pages;
+	rm -rf ../python_html;
 	mv  docs_rst/build/html/ ../python_html;
-	git add python_html;
+	git add ../python_html;
 	git commit -m "Bringing python docs up to date with $ONSHAPE_CLIENTS_VERSION";
 	git push origin HEAD;
 	git checkout master;
