310298fec7fb94d46b1db5cdc936086fe3c04104
Ethan Keller <ekeller@onshape.com>
2019-08-20 16:24:53 -0400

    Added bulk metadata change example.

--- python/onshape_client/example_programs/import_file.py
+++ python/onshape_client/example_programs/import_file.py
@@ -14,7 +14,7 @@ import time
 def import_file(file_path, did, wid):
     """Import a file from the local file system. Returns the URL of the resulting element if translated."""
     client = Client.get_client()
-    file = open(file_path, 'r').read()
+    file = open(file_path, 'rb').read()
     r = client.blob_elements_api.upload_file_create_element(did, wid, media_type="application/stl", file=file, translate=True, encodedFilename=file_path.split('/')[-1], _preload_content=False)
     translation_id = get_field(r, 'translationId')
     print("The translationId is: {}.".format(translation_id))
--- python/onshape_client/example_programs/set_metadata.py
+++ python/onshape_client/example_programs/set_metadata.py
@@ -1,6 +1,34 @@
 from onshape_client.client import Client
+from onshape_client.utility import load_json
 import json
+from datetime import datetime
 
+# For updating many properties of many elements
+class BulkMetadataUpdate(object):
+    def __init__(self, primary_metadata_body):
+        # The primary metadata_body defines the wvm against which to update the metadata
+        self.primary_metadata_body = primary_metadata_body
+        self.additional_metadata_calls = []
+
+    def add_metadata_body(self, metadata_body):
+        self.additional_metadata_calls.append(metadata_body)
+
+    def get_merged_bodies(self):
+        body = self.primary_metadata_body.body
+        #merge the bodies
+        for metadata_body in self.additional_metadata_calls:
+            body['items'].extend(metadata_body.body['items'])
+        return body
+
+    def send(self):
+        body = self.get_merged_bodies()
+        onshape_element = self.primary_metadata_body.onshape_element
+        self.metadata = Client.get_client().metadata_api.update_wv_metadata(onshape_element.did,
+                                                                            onshape_element.wvm,
+                                                                            onshape_element.wvmid, body, _preload_content=False)
+
+
+# For updating multiple properties of a single Onshape element
 class MetaDataBody(object):
 
     def __init__(self, onshape_element):
@@ -87,7 +115,7 @@ class MetaDataBody(object):
         part_index_to_part_id = []
         el = self.onshape_element
         response = client.metadata_api.get_wmve_ps_metadata(el.did, el.wvm, el.wvmid, el.eid, _preload_content=False)
-        data = json.loads(response.data)
+        data = load_json(response)
         for i, part in enumerate(data['items']):
             partId = part['partId']
             part_index_to_part_id.append(partId)
@@ -108,10 +136,46 @@ class MetaDataBody(object):
         self.metadata = Client.get_client().metadata_api.update_wv_metadata(self.onshape_element.did, self.onshape_element.wvm,
                                                                self.onshape_element.wvmid, self.body)
 
-if __name__ == "__main__":
+
+def update_one_element_metadata_example():
     from onshape_client.onshape_url import OnshapeElement
+    print("Now updating a single property within a single element")
     element = OnshapeElement(
-        "http://localhost.dev.onshape.com:8080/documents/ee93985ceb928f7a7c79fed3/w/593971edd8336a08489ec7c0/e/c066cfbaf68e866b53cdb73f")
+        "https://cad.onshape.com/documents/31e9fe5e53feb43633f360f4/w/052a282a976da47ba4c305dd/e/a270fe6f3d3ddc0dd6aee29c")
     metadata_class = MetaDataBody(element)
-    metadata_class.add_to_part_metadata("JHD", "Part number", "my glorious part number")
+    metadata_class.add_to_part_metadata("Part number", "my glorious part number", pid="JHD")
     metadata_class.send()
+
+
+def update_many_element_metadata_example():
+    from onshape_client.onshape_url import OnshapeElement
+    print("Now updating multiple properties in many elements")
+
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    element1 = OnshapeElement(
+        "https://cad.onshape.com/documents/31e9fe5e53feb43633f360f4/w/052a282a976da47ba4c305dd/e/a270fe6f3d3ddc0dd6aee29c")
+    metadata_class1 = MetaDataBody(element1)
+    metadata_class1.add_to_part_metadata("Part number", "my glorious part number! " + time, pid="JHD")
+
+    element2 = OnshapeElement(
+        "https://cad.onshape.com/documents/31e9fe5e53feb43633f360f4/w/052a282a976da47ba4c305dd/e/d329ef11b727b097da599eb6")
+    metadata_class2 = MetaDataBody(element2)
+    metadata_class2.add_to_part_metadata("Description", "my wicked desciption! " + time, pid="JHD")
+
+    element3 = OnshapeElement(
+        "https://cad.onshape.com/documents/31e9fe5e53feb43633f360f4/w/052a282a976da47ba4c305dd/e/e252cef9464938dc28356295")
+    metadata_class3 = MetaDataBody(element3)
+    metadata_class3.add_to_part_metadata("Name", "my amazing name! " + time, pid="JHD")
+
+    bulk_metadata = BulkMetadataUpdate(metadata_class1)
+    bulk_metadata.add_metadata_body(metadata_class2)
+    bulk_metadata.add_metadata_body(metadata_class3)
+
+    bulk_metadata.send()
+
+if __name__ == "__main__":
+    update_one_element_metadata_example()
+    update_many_element_metadata_example()
+
+
