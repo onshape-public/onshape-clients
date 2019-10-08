18fbf2b06451d3b1033e92df71dc978c07c7eb77
Ethan Keller <ekeller@onshape.com>
2019-10-08 10:53:54 -0400

    Added version 1 mediatype accepts to the part studios api

--- python/test/test_part_studios_api.py
+++ python/test/test_part_studios_api.py
@@ -1,4 +1,6 @@
 from onshape_client.oas.models.bt_id_translation_params import BTIdTranslationParams
+import json
+
 
 def test_translate_ids(configurable_cubes, client):
     sketch_id = "JCF"
@@ -7,3 +9,8 @@ def test_translate_ids(configurable_cubes, client):
     result = client.part_studios_api.translate_ids(cube.did, cube.wvm, cube.wvmid, cube.eid, params)
     assert result.ids[0].source == sketch_id
     assert result.ids[0].target[0] == sketch_id
+
+def test_get_features(cube, client):
+    result = client.part_studios_api.get_features1(cube.did, cube.wvm, cube.wvmid, cube.eid, _preload_content=False)
+    data = json.loads(result.data)
+    assert data['features'][0]['message']['name'] == 'Sketch 1'
