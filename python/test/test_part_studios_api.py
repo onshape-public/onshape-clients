df8eb044011a1574ee57bcec9c64da308e021dd3
Ethan Keller <ekeller@onshape.com>
2020-01-30 12:06:56 -0500

    Add copy workspace and sketch circle creation test

--- python/test/test_part_studios_api.py
+++ python/test/test_part_studios_api.py
@@ -1,5 +1,5 @@
 from onshape_client.oas import BTMParameterString149, BTMSketch151, BTMSketchConstraint2, BTMSketchCurveSegment155, \
-    BTMIndividualQuery138, BTMParameterQueryList148, BTMSketchPoint158, BTCurveGeometryLine117
+    BTMIndividualQuery138, BTMParameterQueryList148, BTMSketchPoint158, BTCurveGeometryLine117, BTMSketchCurve4, BTCurveGeometryCircle115
 from onshape_client.oas.models.bt_feature_definition_call1406 import BTFeatureDefinitionCall1406
 from onshape_client.oas.models.bt_id_translation_params import BTIdTranslationParams
 from onshape_client.oas.models.btm_feature134 import BTMFeature134
@@ -58,14 +58,14 @@ def test_insert_line_sketch(client, part_studio):
     POINT_ID = "myPoint"
 
     point_string_param = BTMParameterString149(value=POINT_ID, parameter_id="localFirst")
-    line_end_string_param = BTMParameterString149(value=f"{LINE_ID}.{START}", parameter_id="localSecond")
+    line_end_string_param = BTMParameterString149(value=f"{LINE_ID}.{START}", parameter_id="localSecond", bt_type="BTMParameterString-149")
     coincident_constraint = BTMSketchConstraint2(constraint_type="COINCIDENT", parameters=[point_string_param, line_end_string_param], entity_id="constrainId", bt_type="BTMSketchConstraint-2")
-    point = BTMSketchPoint158(y=-0.071735, x=-0.0564367610245, is_user_point=True, is_construction=False, entity_id=POINT_ID)
+    point = BTMSketchPoint158(y=-0.071735, x=-0.0564367610245, entity_id=POINT_ID)
     line_geometry = BTCurveGeometryLine117(pnt_x=0., pnt_y=0., dir_x=0.5, dir_y=0.5)
     line = BTMSketchCurveSegment155(start_point_id=f"{LINE_ID}.{START}", end_point_id=f"{LINE_ID}.{END}", start_param= 0., end_param=1., geometry=line_geometry, entity_id=LINE_ID)
     sketch = BTMSketch151(entities=[line, point], name="My New Line", parameters=[plane_query], constraints=[coincident_constraint])
     feature_definition = BTFeatureDefinitionCall1406(feature=sketch)
-    client.part_studios_api.add_part_studio_feature(did=part_studio.did, wvm=part_studio.wvm, wvmid=part_studio.wvmid, eid=part_studio.eid, bt_feature_definition_call1406_bt_feature_definition_call1406=feature_definition)
+    client.part_studios_api.add_part_studio_feature(did=part_studio.did, wvm=part_studio.wvm, wvmid=part_studio.wvmid, eid=part_studio.eid, bt_feature_definition_call1406=feature_definition)
     features = client.part_studios_api.get_part_studio_features(did=part_studio.did, wvm=part_studio.wvm, wvmid=part_studio.wvmid, eid=part_studio.eid)
     assert features.features[0].name == FEATURE_NAME
 
@@ -82,3 +82,14 @@ def test_create_surface(client, part_studio):
                                                     eid=part_studio.eid,
                                                     bt_feature_definition_call1406_bt_feature_definition_call1406=feature_definition)
 
+def test_create_circle(client, part_studio):
+    FEATURE_NAME = "My New Circle"
+    PLANE_ID = "JDC" # The plane deterministic ID for the sketch
+    plane_query = BTMParameterQueryList148(parameter_id="sketchPlane", queries=[BTMIndividualQuery138(deterministic_ids=[PLANE_ID])])
+    CIRCLE_ID = "myCircle"
+
+    circle_geometry = BTCurveGeometryCircle115(radius=0.5, xcenter=0.0, ycenter=0.0, xdir=1.0, ydir=0.0, clockwise=False)
+    circle = BTMSketchCurve4(center_id=f"{CIRCLE_ID}.center", entity_id=CIRCLE_ID, geometry=circle_geometry)
+    sketch = BTMSketch151(entities=[circle], name=FEATURE_NAME, parameters=[plane_query])
+    feature_definition = BTFeatureDefinitionCall1406(feature=sketch)
+    client.part_studios_api.add_part_studio_feature(did=part_studio.did, wvm=part_studio.wvm, wvmid=part_studio.wvmid, eid=part_studio.eid, bt_feature_definition_call1406_bt_feature_definition_call1406=feature_definition)
