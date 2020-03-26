433220be03a0378316658bccdc7497954499d555
Ethan Keller <ekeller@onshape.com>
2020-03-26 16:42:08 -0400

    build(cli): adds generate command and go client support
    a bunch of small changes to how the cli is put together that prioritize easily adding new clients.
    Also made the testing terser as well as more comprehensive.

--- cli/cli/clientPackage.py
+++ cli/cli/clientPackage.py
@@ -1,7 +1,6 @@
 """All client package classes."""
 from pathlib import Path
 import re
-import os
 import subprocess
 import shutil
 
@@ -10,58 +9,127 @@ class CliError(Exception):
     pass
 
 
+class ClientPackageMeta:
+    """Decorators and the like for defining aspects of the ClientPackage."""
+
+    @staticmethod
+    def action(func):
+        """Decorator to mark the method as an action that can be taken on this client. Only for instance methods."""
+
+        def wrapper(self, *args, **kwargs):
+            self.print_divider(f"{func.__name__}ing the {self.name} client.")
+            func(self, *args, **kwargs)
+
+        return wrapper
+
+
 class ClientPackage:
-    """A class representing a single client package to facilitate changing version number and publishing."""
+    """Representing a single client package to facilitate changing version number and publishing."""
 
     name = "generic"
-    version_regex = r"\d*\.\d*\.\d*[\w-]"
 
     def __init__(
-        self, root_path, file_path_to_version_identifier, regex_for_version_number
+        self,
+        repo=Path.cwd(),
+        version_regex=r"\d*\.\d*\.\d*[\w-]",
+        oas_client_name=None,
+        dry_run=False,
     ):
+        """ The base class for client packages. Only things necessary for all clients should go here.
+        :param String version_regex: ex "\d*\.\d*\.\d*[\w-]" valid version regex
+        :param String name: the name of the client - also the name of the subfolder used to hold the client.
+        :param String oas_client_name: the OpenApi generator client name. ex: python-experimental
+        :param Boolean dry_run: If set to true, no commands are actually ran, but it will show which commands would be
+        ran.
         """
-        :param Path root_path: ex __file__/python path object
-        :param Path file_path_to_version_identifier: ex "setup.py"
-        :param String regex_for_version_number: ex r'version=".*"'
-        """
-        self.root_path = root_path
-        self.file_path_to_version_identifier = file_path_to_version_identifier
-        self.regex_for_version_number = regex_for_version_number
+        repo = Path(repo)
+        if repo.name != "onshape-clients":
+            raise CliError(
+                f"Most likely calling from the wrong location. Should be in a directory named 'onshape-clients' but "
+                f"instead in '{repo.name}'"
+            )
+        name = self.name
+        self.root_path = repo / name
+        self.version_regex = version_regex
+        self.oas_client_name = oas_client_name if oas_client_name else name
+        self.dry_run = dry_run
 
-    def set_version(self, version):
+    def __getattr__(self, name):
+        def skip_func(*args, **kwargs):
+            print(f"'{name}' is not supported by client '{self.name}' - skipping")
+
+        return skip_func
+
+    @ClientPackageMeta.action
+    def generate(self):
+        """Generate the client with default options. Per client options should be set in
+        ./<CLIENT_FOLDER>/openapi_config.json"""
+        self.run(
+            f"openapi-generator-cli generate -i ./openapi.json --skip-validate-spec -g {self.oas_client_name} -o {self.root_path} "
+            f"-c {self.root_path / 'openapi_config.json'}",
+            cwd=self.root_path.parent,
+        )
+
+    def set_version_in_source(
+        self,
+        version="0.0.0",
+        file_path_to_version_identifier=None,
+        regex_for_version_number=None,
+    ):
+        """Set the version for clients that include the version number in their source files.
+        :param version: String The version to update to: ex "1.1.2"
+        :param file_path_to_version_identifier: Path ex "setup.py"
+        :param regex_for_version_number: String ex r'version=".*"'"""
+        if not all(
+            [version, file_path_to_version_identifier, regex_for_version_number]
+        ):
+            raise CliError("Must specify all parameters.")
         if not version or not re.match(self.version_regex, version):
             raise CliError(
                 f"Version specified: {version} does not match regex {self.version_regex}."
             )
-        f = self.file_path_to_version_identifier.open().read()
-        result = re.sub(self.regex_for_version_number, version, f)
-        self.file_path_to_version_identifier.open(mode="w").write(result)
+        f = file_path_to_version_identifier.open().read()
+        result = re.sub(regex_for_version_number, version, f)
+        file_path_to_version_identifier.open(mode="w").write(result)
 
-    def publish(self):
-        print(f"Publishing the {self.name} client.")
-
-    def test(self):
-        print(f"Testing the {self.name} client.")
-
-    def run(self, command):
+    def run(self, command, cwd=None):
         """Run a command in the shell for this client."""
-        print(f'Running command "{command}"')
-        return subprocess.run(command.split(" "), cwd=self.root_path)
+        self.print_divider(
+            f'{"Running" if not self.dry_run else "Would run"} command "{command}"'
+        )
+        command = command.split(" ")
+        # Dry runs should pretend the command succeeded.
+        if self.dry_run:
+            return subprocess.CompletedProcess(command, 0)
+        return subprocess.run(command, cwd=cwd if cwd else self.root_path)
+
+    def print_divider(self, message):
+        print(f"============= {message} ===============")
+
+
+class GoPackage(ClientPackage):
+    name = "go"
 
 
 class PythonPackage(ClientPackage):
     name = "python"
 
-    def __init__(self, repo=Path.cwd()):
-        python_package_path = Path(repo) / "python"
+    def __init__(self, **kwargs):
         super().__init__(
-            python_package_path,
-            python_package_path / "setup.py",
-            r'(?<=version=")' + self.version_regex + r'(?=",)',
+            oas_client_name="python-experimental", **kwargs,
         )
 
+    def set_version(
+        self, **kwargs,
+    ):
+        return self.set_version_in_source(
+            file_path_to_version_identifier=self.root_path / "setup.py",
+            regex_for_version_number=r'(?<=version=")' + self.version_regex + r'(?=",)',
+            **kwargs,
+        )
+
+    @ClientPackageMeta.action
     def publish(self):
-        super().publish()
         setup = self.root_path / "setup.py"
         dist = self.root_path / "dist"
         dist.mkdir(exist_ok=True, parents=True)
@@ -72,14 +140,17 @@ class PythonPackage(ClientPackage):
             raise CliError("Error uploading client to pypi.")
         shutil.rmtree(str(dist))
 
+    @ClientPackageMeta.action
     def test(self, marker=None):
         result = self.run(f"pipenv run pytest {f'-m {marker}' if marker else ''}")
         if result.returncode != 0:
             raise CliError("Error testing client.")
 
+    @ClientPackageMeta.action
     def install(self):
         self.run(f"pipenv install {self.root_path} --dev")
 
+    @ClientPackageMeta.action
     def lint(self, fix=False):
         """Lint files. If fix is given, write the files back."""
-        self.run(f"black {self.root_path}{' --check' if not fix else ''}")
+        self.run(f"black {self.root_path}{' --check' if fix else ''}")
--- cli/tests/test_cli.py
+++ cli/tests/test_cli.py
@@ -1,19 +1,22 @@
 from click.testing import CliRunner
 from cli.cli import entry
+import click
 from cli.clientPackage import CliError
+import os
 from pathlib import Path
-import time
 import subprocess
+import pytest
 
 runner = CliRunner()
 repo = Path(__file__).parent.parent.parent.absolute()
 
 
-def invoke(command):
+def invoke(command, dry_run=True):
+    command = f"-r {repo}{' --dry-run' if dry_run else ''} {command}"
+    print(f"Running command 'onshape-clients {command}'")
     command = command.split(" ")
-    command.insert(0, "-r")
-    command.insert(1, str(repo))
-    return runner.invoke(entry, command)
+    result = runner.invoke(entry, command)
+    assert result.exit_code == 0
 
 
 def test_linted():
@@ -23,28 +26,23 @@ def test_linted():
     assert result.returncode == 0
 
 
-def test_entry_no_version():
-    """onshape-clients publish
-    """
-    result = invoke("publish")
+def test_help():
+    """Ensures just blank help request works."""
+    runner.invoke(entry)
+
+
+def test_wrong_folder_errors():
+    """If outside onshape-clients, this should error."""
+    cwd = Path.cwd()
+    os.chdir(str(Path.home()))
+    result = runner.invoke(entry, "lint")
+    assert result.exit_code == 1
     assert result.exc_info[0] == CliError
+    os.chdir(str(cwd))
 
 
-def test_entry_duplicate_version():
-    """onshape-clients -c python publish -v 0.0.0
+@pytest.mark.parametrize("command", [command for command in entry.commands])
+def test_blank_commands(command):
+    """Ensure all commands with defaults work.
     """
-    result = invoke("-c python publish -v 0.0.0",)
-    assert result.exc_info[0] == CliError
-    assert result.exc_info[1].args[0] == "Error uploading client to pypi."
-
-
-def test_test_function():
-    """ The "fast" tests should take no more than 20 seconds per client.
-    onshape-clients test fast
-    """
-    result = invoke("install")
-    assert result.exit_code == 0
-    start = time.time()
-    result = invoke("test -m fast")
-    assert result.exit_code == 0
-    assert time.time() - start < 20
+    invoke(command)
